3fdfbad08ac4334590ad7bc3707ad3f9d45ba984
dnsndrvsk <dnsndrvsk@gmail.com>
2017-06-10 20:38:54 +0300

    'first_editing'

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 'use strict';
  
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css');
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/style.sass')
+  return gulp.src('./sass/main.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./css'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/style.scss', ['sass']);
 });
